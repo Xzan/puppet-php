66af62289d6ee3de5015cdf6d265bf808516d03a
Xzan <xzan@xzan.be>
2012-09-27 16:27:52 +0200

    APC + RFC 1867

--- manifests/init.pp
+++ manifests/init.pp
@@ -11,10 +11,21 @@ class php{
 	package {"php5-xdebug" :
 		ensure => installed,
 	}
+	
+	package {"php-apc" :
+		ensure => installed,
+		require => Package["php5"],
+		notify => Exec["apc-rfc1867"]
+	}
+	
+	Exec {"apc-rfc1867" :
+		command => "echo 'apc.rfc1867 = 1' >> /etc/php5/conf.d/apc.ini"
+		unless => "cat /etc/php5/conf.d/apc.ini | grep -e 'apc.rfc1867 \?= \?1'";
+		notify => Service["apache2"]
+	}
 
 	package {"libapache2-mod-php5" :
 		ensure => installed,
 		require => Package["php5"]
 	}
-
 }
